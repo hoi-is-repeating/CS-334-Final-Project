7129661201e1b40a3cd9f05657e771952ad36113
Olivia Kim <hoiisrepeating@gmail.com>
2024-04-17 18:52:04 -0400

    added y

--- featureSelection.py
+++ featureSelection.py
@@ -6,9 +6,9 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
 #olivia kim
-def split(df):
-    xTrain, xTest = train_test_split(df, test_size=0.3)
-    return xTrain, xTest
+def split(xFeat,y):
+    xTrain, xTest, yTrain, yTest = train_test_split(xFeat,y, test_size=0.3)
+    return xTrain, xTest, yTrain, yTest
 
 
 def select_features(xTrain, xTest):
@@ -23,16 +23,17 @@ def select_features(xTrain, xTest):
 def main():
     
     df = pd.read_csv("data.csv")
+    y = df['label']
+    y = preprocessing.onehot(y,'label')
     df = df.drop(columns=['index','id.orig_h',"id.resp_h","service","missed_bytes","history","label"])
     df = preprocessing.remove_na(df)
     encode_columns = ["id.resp_p",
                       "proto",
                       "conn_state"
-                      
                       ]   
     df = preprocessing.onehot(df,encode_columns)
     df = preprocessing.fill_na(df)
-    xTrain, xTest = split(df)
+    xTrain, xTest, yTrain, yTest = split(df, y)
     prep_columns = ["id.orig_p",
                     "duration",
                     "orig_bytes",
@@ -44,14 +45,18 @@ def main():
                     "resp_ip_bytes"
                     ]
     xTrain, xTest = preprocessing.minmax_range(xTrain,xTest,prep_columns)
-    
-    xTrain, xTest = select_features(xTrain, xTest)   
     plt.figure()
+    sns.heatmap(xTrain.corr(), annot=False, cmap='coolwarm',annot_kws={"size": 5})
+    plt.title('Pearson Correlation Matrix')
+    plt.show()
+    xTrain, xTest = select_features(xTrain, xTest)   
     sns.heatmap(xTrain.corr(), annot=True, cmap='coolwarm',annot_kws={"size": 5})
     plt.title('Pearson Correlation Matrix')
     plt.show() 
     xTrain.to_csv("xTrain.csv", index=False)
     xTest.to_csv("xTest.csv", index=False)
+    yTrain.to_csv("yTrain.csv", index=False)
+    yTest.to_csv("yTest.csv", index=False)
 
 if __name__ == "__main__":
     main()
