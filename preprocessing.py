95d61d714155a00b00949d7f49165d7f69f603eb
Thalia Papageorgiou <tpapage@emory.edu>
2024-04-20 14:25:32 -0400

    hopefully everything is an integer now

--- preprocessing.py
+++ preprocessing.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import sklearn.preprocessing as sk
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
 
 
 #olivia kim
@@ -30,9 +32,11 @@ def main():
     df = remove_na(df)
     print(df)
     df = onehot(df, ['label', 'proto', 'service', 'conn_state', 'history'])
+    df['id.orig_h'] = LabelEncoder().fit_transform(df['id.orig_h'])
+    df['id.resp_h'] = LabelEncoder().fit_transform(df['id.resp_h'])
+
     print(df)
     #checking to see if all the columns are numeric
-    print(df.dtypes)
     #do we need scaling? 
     df.drop(columns=['label_Malicious'], inplace=True)
 
@@ -51,10 +55,10 @@ def main():
     y = df['label_Benign']
     x = df.drop(columns=['label_Benign'])
     xTrain, xTest, yTrain, yTest = train_test_split(x, y, test_size=0.2)
-    # xTrain.to_csv('xTrain.csv', index=False)
-    # xTest.to_csv('xTest.csv', index=False)
-    # yTrain.to_csv('yTrain.csv', index=False)
-    # yTest.to_csv('yTest.csv', index=False)
+    xTrain.to_csv('xTrain.csv', index=False)
+    xTest.to_csv('xTest.csv', index=False)
+    yTrain.to_csv('yTrain.csv', index=False)
+    yTest.to_csv('yTest.csv', index=False)
 
 
 if __name__ == "__main__":
